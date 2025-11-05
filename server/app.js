66a2f0fca1b82ab01caa18cc60237ebc456a16e3
Jeevan kumar <jeevankumarkorra2005@gmail.com>
2025-11-06 03:14:21 +0530

    REMOVED THE app.listen() BLOCK, beacuse vercel can able to handle it

--- server/app.js
+++ server/app.js
@@ -1,18 +1,16 @@
-require('dotenv').config();
+// src/app.js
 
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const apiRoutes = require('./src/routes/api');
 
 const app = express();
-const PORT = process.env.PORT || 4000;
-
-
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+
+module.exports = app;
