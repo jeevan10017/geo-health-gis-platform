0a180ca85409dbd8dfee4bf28e7f18a9e9530dd7
Jeevan kumar <jeevankumarkorra2005@gmail.com>
2025-11-06 05:56:12 +0530

    allowered CORS origin

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-// src/app.js
+// server/app.js
 
 require('dotenv').config();
 const express = require('express');
@@ -6,11 +6,35 @@ const cors = require('cors');
 const apiRoutes = require('./src/routes/api');
 
 const app = express();
-app.use(cors());
+const PORT = process.env.PORT || 4000;
+
+
+const allowedOrigins = [
+  'https://geo-health-backend.vercel.app',
+  'http://localhost:5173'
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
+-
+
 app.use(express.json());
 
 app.use('/api', apiRoutes);
 
-
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is running locally on http://localhost:${PORT}`);
+  });
+}
 
 module.exports = app;
